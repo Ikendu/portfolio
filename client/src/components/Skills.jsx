612fdecb0bfa3122795009ac977926f29c71b934
Chibundu <ndubest56@gmail.com>
2025-01-03 18:16:22 +0100

    more skills

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -1,76 +1,67 @@
-import { Col, Container, Row } from "react-bootstrap";
-import { Facebook, Twitter } from "react-bootstrap-icons";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import colorSharp from "../assets/img/color-sharp.png";
-import { carouselItems, responsive, skillsIcons } from "../data";
+import { Col, Container, Row } from 'react-bootstrap'
+import { Facebook, Twitter } from 'react-bootstrap-icons'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+import colorSharp from '../assets/img/color-sharp.png'
+import { carouselItems, responsive, skillsIcons } from '../data'
 
-const skills = skillsIcons.filter((skill) => skill.id < 4);
-const skillss = skillsIcons.filter((skill) => skill.id > 3 && skill.id < 7);
-const skillsss = skillsIcons.filter((skill) => skill.id > 6 && skill.id < 10);
-const skillssss = skillsIcons.filter((skill) => skill.id > 9 && skill.id < 13);
+const skills = skillsIcons.filter((skill) => skill.id < 4)
+const skillss = skillsIcons.filter((skill) => skill.id > 3 && skill.id < 7)
+const skillsss = skillsIcons.filter((skill) => skill.id > 6 && skill.id < 10)
+const skillssss = skillsIcons.filter((skill) => skill.id > 9 && skill.id < 14)
 
 function SkillSet({ skillSet }) {
   return (
     <ul>
       {skillSet.map((skill, idx) => (
-        <li key={idx} className="flex gap-2 my-2">
-          <img src={skill.icon} className=" bg-white rounded-full w-5" />
+        <li key={idx} className='flex gap-2 my-2'>
+          <img src={skill.icon} className=' bg-white rounded-full w-5' />
           <span>{skill.name} </span>
         </li>
       ))}
     </ul>
-  );
+  )
 }
 
 export default function Skills() {
   return (
-    <section className="skill" id="skills">
+    <section className='skill' id='skills'>
       <Container>
         <Row>
           <Col>
-            <div className="skill-bx p-3 py-5 flex flex-column align-items-center gap-3 md:gap-20">
+            <div className='skill-bx p-3 py-5 flex flex-column align-items-center gap-3 md:gap-20'>
               <h2>Skills</h2>
-              <div className="">
-                <div className="">
-                  <b className="text-2xl text-lime-300">
-                    LIBRARIES/FRAMEWORKS:
-                  </b>
+              <div className=''>
+                <div className=''>
+                  <b className='text-2xl text-lime-300'>LIBRARIES/FRAMEWORKS:</b>
                   <br />
-                  <div className="flex my-4">
-                    <div className="flex gap-5 flex-wrap mx-4 justify-content-between md:w-[750px]">
+                  <div className='flex my-4'>
+                    <div className='flex gap-5 flex-wrap mx-4 justify-content-between md:w-[750px]'>
                       <SkillSet skillSet={skills} />
                       <SkillSet skillSet={skillss} />
                       <SkillSet skillSet={skillsss} />
                       <SkillSet skillSet={skillssss} />
                     </div>
-                    <div className="flex gap-20"></div>
+                    <div className='flex gap-20'></div>
                   </div>
                 </div>
 
-                <p className="flex flex-column gap-3 my-5">
-                  <b className="text-[1.3rem] text-lime-300">
-                    PROGRAMMING LANGUAGES:
-                  </b>
-                  <span className=" text-2xl">
+                <p className='flex flex-column gap-3 my-5'>
+                  <b className='text-[1.3rem] text-lime-300'>PROGRAMMING LANGUAGES:</b>
+                  <span className=' text-2xl'>
                     JavaScript, Java, Typescript, Ruby, PHP, C, Python
                   </span>
                 </p>
 
-                <p className="flex flex-column gap-3">
-                  <span className="text-2xl text-lime-300 ">OTHER SKILLS:</span>
-                  Computer Software/OS Maintenance, Graphics Design, ICT
-                  Personnel
+                <p className='flex flex-column gap-3'>
+                  <span className='text-2xl text-lime-300 '>OTHER SKILLS:</span>
+                  Computer Software/OS Maintenance, Graphics Design, ICT Personnel
                 </p>
               </div>
-              <h2 className="tool-set">Tool-Set and Proficiency level</h2>
-              <Carousel
-                responsive={responsive}
-                infinite={true}
-                className="skill-slider"
-              >
+              <h2 className='tool-set'>Tool-Set and Proficiency level</h2>
+              <Carousel responsive={responsive} infinite={true} className='skill-slider'>
                 {carouselItems.map((item, idx) => (
-                  <div key={idx} className="item">
+                  <div key={idx} className='item'>
                     <img src={item.image} />
                     {/* <div className="img ">{item.image}</div> */}
                     <h5>{item.text}</h5>
@@ -81,7 +72,7 @@ export default function Skills() {
           </Col>
         </Row>
       </Container>
-      <img src={colorSharp} className="background-image-left" />
+      <img src={colorSharp} className='background-image-left' />
     </section>
-  );
+  )
 }
--- client/src/data.jsx
+++ client/src/data.jsx
@@ -1,31 +1,32 @@
-import { Col, Container, Row } from "react-bootstrap";
-import { Facebook, Twitter } from "react-bootstrap-icons";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import meter55 from "./assets/meter/meter55.png";
-import meter65 from "./assets/meter/meter65.png";
-import meter75 from "./assets/meter/meter75.png";
-import meter80 from "./assets/meter/meter80.png";
-import meter85 from "./assets/meter/meter85.png";
-import meter90 from "./assets/meter/meter90.png";
-import meter95 from "./assets/meter/meter95.png";
-import colorSharp from "./assets/img/color-sharp.png";
-import nextjsIcon from "./assets/icons/nextjsIcon.svg";
-import tailwind from "./assets/icons/Tailwind.svg";
-import diamondIcon from "./assets/icons/diamond.svg";
-import nodejs from "./assets/icons/nodejs.svg";
-import mongodb from "./assets/icons/mongodb.svg";
-import FacebookIcon from "./assets/icons/FacebookIcon";
-import bootstrap from "./assets/icons/bootstrap.svg";
-import express from "./assets/icons/express.svg";
-import html from "./assets/icons/html.svg";
-import web from "./assets/icons/express.svg";
-import socket from "./assets/icons/socket.svg";
-import react from "./assets/icons/react.svg";
-import sass from "./assets/icons/sass.svg";
-import css from "./assets/icons/css.svg";
+import { Col, Container, Row } from 'react-bootstrap'
+import { Facebook, Twitter } from 'react-bootstrap-icons'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+import meter55 from './assets/meter/meter55.png'
+import meter65 from './assets/meter/meter65.png'
+import meter75 from './assets/meter/meter75.png'
+import meter80 from './assets/meter/meter80.png'
+import meter85 from './assets/meter/meter85.png'
+import meter90 from './assets/meter/meter90.png'
+import meter95 from './assets/meter/meter95.png'
+import colorSharp from './assets/img/color-sharp.png'
+import nextjsIcon from './assets/icons/nextjsIcon.svg'
+import tailwind from './assets/icons/Tailwind.svg'
+import diamondIcon from './assets/icons/diamond.svg'
+import nodejs from './assets/icons/nodejs.svg'
+import mongodb from './assets/icons/mongodb.svg'
+import FacebookIcon from './assets/icons/FacebookIcon'
+import bootstrap from './assets/icons/bootstrap.svg'
+import express from './assets/icons/express.svg'
+import html from './assets/icons/html.svg'
+import web from './assets/icons/express.svg'
+import socket from './assets/icons/socket.svg'
+import react from './assets/icons/react.svg'
+import sass from './assets/icons/sass.svg'
+import css from './assets/icons/css.svg'
+import django from './assets/icons/django.svg'
 
-FacebookIcon;
+FacebookIcon
 
 export const carouselItems = [
   {
@@ -46,7 +47,7 @@ export const carouselItems = [
   },
   {
     image: meter55,
-    text: `Nextjs`,
+    text: `Django`,
   },
   {
     image: meter75,
@@ -64,21 +65,22 @@ export const carouselItems = [
     image: meter75,
     text: `Express`,
   },
-];
+]
 export const skillsIcons = [
-  { id: 1, name: "Reactjs", icon: react },
-  { id: 2, name: "Nextjs", icon: nextjsIcon },
-  { id: 3, name: "HTML", icon: html },
-  { id: 4, name: "Tailwind", icon: tailwind },
-  { id: 5, name: "Bootstrap", icon: bootstrap },
-  { id: 6, name: "CSS", icon: css },
-  { id: 7, name: "Nodejs", icon: nodejs },
-  { id: 8, name: "MongoDB", icon: mongodb },
-  { id: 9, name: "Express", icon: express },
-  { id: 10, name: "Web RTC", icon: web },
-  { id: 11, name: "Socket.io", icon: socket },
-  { id: 12, name: "Sass", icon: sass },
-];
+  { id: 1, name: 'Reactjs', icon: react },
+  { id: 2, name: 'Nextjs', icon: nextjsIcon },
+  { id: 3, name: 'HTML', icon: html },
+  { id: 4, name: 'Tailwind', icon: tailwind },
+  { id: 5, name: 'Bootstrap', icon: bootstrap },
+  { id: 6, name: 'CSS', icon: css },
+  { id: 7, name: 'Nodejs', icon: nodejs },
+  { id: 8, name: 'MongoDB', icon: mongodb },
+  { id: 9, name: 'Express', icon: express },
+  { id: 10, name: 'Web RTC', icon: web },
+  { id: 11, name: 'Socket.io', icon: socket },
+  { id: 12, name: 'Sass', icon: sass },
+  { id: 13, name: 'Django', icon: django },
+]
 
 export const responsive = {
   superLargeDesktop: {
@@ -98,27 +100,27 @@ export const responsive = {
     breakpoint: { max: 464, min: 0 },
     items: 1,
   },
-};
+}
 
 export const navItems = [
   {
     id: 1,
-    name: "first",
-    icon: ".",
+    name: 'first',
+    icon: '.',
   },
   {
     id: 2,
-    name: "second",
-    icon: ".",
+    name: 'second',
+    icon: '.',
   },
   {
     id: 3,
-    name: "third",
-    icon: ".",
+    name: 'third',
+    icon: '.',
   },
   {
     id: 4,
-    name: "fourth",
-    icon: ".",
+    name: 'fourth',
+    icon: '.',
   },
-];
+]
