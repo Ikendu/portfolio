f6e5a86a268e26ad3bc74ac0c578bbd8124ca287
Chibundu <ndubest56@gmail.com>
2025-01-05 20:44:46 +0100

    more skills

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
-import "./App.css";
-import Banner from "./components/Banner";
-import Navbar from "./components/Navbar";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Skills from "./components/Skills";
-import Project from "./components/Project";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import './App.css'
+import Banner from './components/Banner'
+import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Skills from './components/Skills'
+import Project from './components/Project'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import 'animate.css'
+import TrackVisibility from 'react-on-screen'
 
 function App() {
   return (
@@ -13,11 +15,18 @@ function App() {
       <Navbar />
       <Banner />
       <Skills />
-      <Project />
+      <TrackVisibility>
+        {({ isVisible }) => (
+          <div className={isVisible ? 'animate__animated animate__backInLeft' : ''}>
+            <Project />
+          </div>
+        )}
+      </TrackVisibility>
+
       <Contact />
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -4,8 +4,6 @@ import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
 import colorSharp from '../assets/img/color-sharp.png'
 import { carouselItems, responsive, skillsIcons } from '../data'
-import 'animate.css'
-import TrackVisibility from 'react-on-screen'
 
 const skills = skillsIcons.filter((skill) => skill.id < 5)
 const skillss = skillsIcons.filter((skill) => skill.id > 4 && skill.id < 9)
@@ -31,56 +29,48 @@ export default function Skills() {
       <Container>
         <Row>
           <Col>
-            <TrackVisibility>
-              {({ isVisible }) => (
-                <div
-                  className={
-                    isVisible
-                      ? 'skill-bx p-3 py-5 flex flex-column align-items-center gap-3 md:gap-20 animate__animated animate__backInLeft'
-                      : `skill-bx p-3 py-5 flex flex-column align-items-center gap-3 md:gap-20`
-                  }
-                >
-                  <h2>Skills</h2>
-                  <div className=''>
-                    <div className=''>
-                      <b className='text-2xl text-lime-300'>LIBRARIES/FRAMEWORKS:</b>
-                      <br />
-                      <div className='flex my-4'>
-                        <div className='flex gap-5 flex-wrap mx-4 justify-content-between md:w-[750px]'>
-                          <SkillSet skillSet={skills} />
-                          <SkillSet skillSet={skillss} />
-                          <SkillSet skillSet={skillsss} />
-                          <SkillSet skillSet={skillssss} />
-                        </div>
-                        <div className='flex gap-20'></div>
-                      </div>
+            <div
+              className={`skill-bx p-3 py-5 flex flex-column align-items-center gap-3 md:gap-20`}
+            >
+              <h2>Skills</h2>
+              <div className=''>
+                <div className=''>
+                  <b className='text-2xl text-lime-300'>LIBRARIES/FRAMEWORKS:</b>
+                  <br />
+                  <div className='flex my-4'>
+                    <div className='flex gap-5 flex-wrap mx-4 justify-content-between md:w-[750px]'>
+                      <SkillSet skillSet={skills} />
+                      <SkillSet skillSet={skillss} />
+                      <SkillSet skillSet={skillsss} />
+                      <SkillSet skillSet={skillssss} />
                     </div>
-
-                    <p className='flex flex-column gap-3 my-5'>
-                      <b className='text-[1.3rem] text-lime-300'>PROGRAMMING LANGUAGES:</b>
-                      <span className=' text-2xl'>
-                        JavaScript, Java, Typescript, Ruby, PHP, C, Python
-                      </span>
-                    </p>
-
-                    <p className='flex flex-column gap-3'>
-                      <span className='text-2xl text-lime-300 '>OTHER SKILLS:</span>
-                      Computer Software/OS Maintenance, Graphics Design, ICT Personnel
-                    </p>
+                    <div className='flex gap-20'></div>
                   </div>
-                  <h2 className='tool-set'>Tool-Set and Proficiency level</h2>
-                  <Carousel responsive={responsive} infinite={true} className='skill-slider'>
-                    {carouselItems.map((item, idx) => (
-                      <div key={idx} className='item'>
-                        <img src={item.image} />
-                        {/* <div className="img ">{item.image}</div> */}
-                        <h5>{item.text}</h5>
-                      </div>
-                    ))}
-                  </Carousel>
                 </div>
-              )}
-            </TrackVisibility>
+
+                <p className='flex flex-column gap-3 my-5'>
+                  <b className='text-[1.3rem] text-lime-300'>PROGRAMMING LANGUAGES:</b>
+                  <span className=' text-2xl'>
+                    JavaScript, Java, Typescript, Ruby, PHP, C, Python
+                  </span>
+                </p>
+
+                <p className='flex flex-column gap-3'>
+                  <span className='text-2xl text-lime-300 '>OTHER SKILLS:</span>
+                  Computer Software/OS Maintenance, Graphics Design, ICT Personnel
+                </p>
+              </div>
+              <h2 className='tool-set'>Tool-Set and Proficiency level</h2>
+              <Carousel responsive={responsive} infinite={true} className='skill-slider'>
+                {carouselItems.map((item, idx) => (
+                  <div key={idx} className='item'>
+                    <img src={item.image} />
+                    {/* <div className="img ">{item.image}</div> */}
+                    <h5>{item.text}</h5>
+                  </div>
+                ))}
+              </Carousel>
+            </div>
           </Col>
         </Row>
       </Container>
