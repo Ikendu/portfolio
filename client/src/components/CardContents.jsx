a6f1c12272bf2b51a92f42b9793234206dd601f6
David Aniede <ndubest56@gmail.com>
2026-01-12 13:20:11 +0100

    restructure skills

--- client/src/components/CardContents.jsx
+++ client/src/components/CardContents.jsx
@@ -1,6 +1,6 @@
 import { Col } from "react-bootstrap";
 
-export default function CardContents({ title, desc, image, link }) {
+export default function CardContents({ title, desc, image, link, tools }) {
   return (
     <Col sm={6} md={4}>
       <a href={link} target="_blank" className="proj-imgbx">
@@ -8,6 +8,7 @@ export default function CardContents({ title, desc, image, link }) {
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{desc}</span>
+          <p className="font-bold pt-4">{tools}</p>
         </div>
       </a>
     </Col>
--- client/src/components/projectAll.js
+++ client/src/components/projectAll.js
@@ -14,6 +14,7 @@ import testProject from "../assets/img/testProject.jpg";
 import testProject1 from "../assets/img/testProject1.jpg";
 import testProject3 from "../assets/img/testProject3.jpg";
 import prime from "../assets/img/prime.jpg";
+import lodge from "../assets/img/lodge.jpg";
 
 // import image3 from "../assets/img/project-img3.png";
 
@@ -21,18 +22,21 @@ export const teamWorks = [
   {
     title: `Nanocode Official website 🏢`,
     desc: `Previous employer's company official website `,
+    tools: "React, Vite, Tailwind CSS, Redux Toolkit, Socket.io, WebRTC etc",
     image: team,
     link: `https://www.nanocodes.com.ng/`,
   },
   {
     title: `Andromedia 💻 `,
     desc: `A software as a service website that focus on customer real time communication`,
+    tools: "React, Vite, Tailwind CSS, Redux Toolkit, Socket.io, RESTful APIs",
     image: team1,
     link: `https://www.andromedia.cc/`,
   },
   {
     title: `Bulloak 🏦`,
     desc: `A financial service for savings, crypto exchange and retirement benefits`,
+    tools: "React, Vite, Tailwind CSS, Redux Toolkit, RESTful APIs",
     image: team2,
     link: `https://bulloakltd.com/`,
   },
@@ -40,26 +44,38 @@ export const teamWorks = [
 
 export const freelanceWorks = [
   {
+    title: `Morelinks Lodging System 🎋`,
+    desc: `A modern lodging website with booking capabilities, user reviews, and virtual tours showcasing comfortable accommodations and exceptional services.`,
+    tools: "React, Vite, Tailwind CSS, PHP, MySQL, RESTful APIs",
+    image: lodge,
+    link: `https://lodge.morelinks.com.ng/`,
+  },
+  {
     title: `Ejimofor Legal Consults 🎋`,
     desc: `A comprehensive hub offering free legal services for victims of human rights abuses and indigent individuals with updates on breaking legal news and local legislation`,
+    tools: "Wordpress, PHP, plugins",
     image: prime,
     link: `https://primelegal.com.ng/`,
   },
   {
     title: `More-Links Deco 🎋`,
     desc: `Elegant decoration website showcasing vibrant interiors, sleek layouts, highlighting creative aesthetics and modern web development skills.`,
+    tools: "React, Vite, Tailwind CSS, PHP, MySQL, RESTful APIs, Bootstrap",
     image: decofront,
     link: `https://moredeco.com.ng/`,
   },
   {
     title: `Oculus Tech 💺`,
     desc: `Futuristic tech website featuring sleek design, bold typography, interactive elements, and cutting-edge visuals.`,
+    tools: "JavaScript, HTML, CSS, PHP, Bootstrap",
     image: oculusFront,
     link: `https://oculustech.com.ng`,
   },
   {
     title: `Gift Hair 👩`,
     desc: `An ecommerce website for selling of hair, attachment and wig`,
+    tools:
+      "React, Vite, Tailwind CSS, Redux Toolkit, RESTful APIs, Nodejs, MongoDB, Bootstrap",
     image: freelance,
     link: `https://gifthairs.netlify.app`,
   },
@@ -69,18 +85,21 @@ export const personalWorks = [
   {
     title: `Basic Calculator 🧮`,
     desc: `For mathematical operations`,
+    tools: "React, Vite, Tailwind CSS",
     image: procalc,
     link: `https://calculatedavid.vercel.app/`,
   },
   {
     title: `Movie List 🎬🎦`,
     desc: `A movie search system that gives you the list of related names of movies searched. `,
+    tools: "React, Vite, Tailwind CSS",
     image: proMovies,
     link: `https://nemovies.netlify.app/`,
   },
   {
     title: `David Portfolio 💼`,
     desc: `List and Description of all past and present design and implentation activities`,
+    tools: "React, Vite, Tailwind CSS",
     image: proFile,
     link: `https://davidaniede.vercel.app/`,
   },
